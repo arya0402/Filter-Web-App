e16b09932e7c655ee8f13b881ee9d90fb36fe569
Aryadolphin <78508957+arya0402@users.noreply.github.com>
2022-07-06 12:37:23 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var noseX = 0;
 var noseY = 0;
 
 function preload() {
-    img = loadImage("https://i.postimg.cc/PxFvYgkv/l1.png");
+    img = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 
 function setup() {
@@ -33,7 +33,7 @@ function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
         noseX = results[0].pose.nose.x - 13;
-        noseY =  results[0].pose.nose.y + 55;
+        noseY =  results[0].pose.nose.y - 55;
         console.log("nose x = " + noseX);
         console.log("nose y = " + noseY);
     }
